48870306660cde8ecded843cfdd6296b7b8f0aa7
thanhdat77 <nguyenthanhdat070701@gmail.com>
2021-11-27 22:18:49 +0700

    choose gallery or camera

--- app/src/main/java/project/petshop/views/ModelActivity.kt
+++ app/src/main/java/project/petshop/views/ModelActivity.kt
@@ -1,25 +1,50 @@
 package project.petshop.views
 
 import android.Manifest
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.ActivityNotFoundException
+import android.content.ContentValues
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.startActivityForResult
 import kotlinx.android.synthetic.main.activity_model.*
 import project.petshop.R
 
 var PERMISSON_CODE = 0
+val REQUEST_IMAGE_CAPTURE = 1
+
 
 class ModelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_model)
-        upload.setOnClickListener(){
+        upload.setOnClickListener {
             requestPermisson()
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).also{
+                it.type = "image/*"
+                startActivityForResult(it, 0)
+
+
+                }
+            }
+        }
+
+    private fun TakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+
         }
     }
-
     private fun hasWriteExternalStoragePermission() =
         ActivityCompat.checkSelfPermission(
             this,
@@ -59,4 +84,17 @@ class ModelActivity : AppCompatActivity() {
 
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == 0){
+            var uri = data?.data
+            imageView.setImageURI(uri)
+        }
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            val imageBitmap = data.extras.get("data") as Bitmap
+//            imageView.setImageBitmap(imageBitmap)
+        }
+    }
+
 }
